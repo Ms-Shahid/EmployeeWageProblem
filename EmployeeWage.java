a93dc1104e336756104f97b54232ae59df0c8f79
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-20 23:36:04 +0530

    Employee present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,7 +4,6 @@ public class EmployeeWage {
 
 	static int IS_FULL_TIME =1;
 	public static void main(String[] args) {
-		
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 		
 		if (empCheck == IS_FULL_TIME) {
