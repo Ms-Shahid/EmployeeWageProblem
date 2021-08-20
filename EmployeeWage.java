5e69c40bf361df12b92f0caf64ca2ba34aa8eac3
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-21 00:32:07 +0530

    total working hrs and days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,23 +6,25 @@ public class EmployeeWage {
 	public static final int IS_PRESENT_HALF = 2;
 	static int Wage_Per_Hour = 20;
 	static int max_days=20;
+	static int max_hrs = 100;
 	public static void main(String[] args) {
 		
-		int Daily_Wage = 0;
 		int Monthly_Wage = 0;
-		int empHrs = 0;
-		for (int day=0; day < max_days; day++){
-	
+		int empHrs=0;
+		int totalempHrs = 0;
+		int workingDays=0;
+		while (totalempHrs <= max_hrs && workingDays <=max_days) {
+			workingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;	
 			switch (empCheck) {
 				case IS_PRESENT_FULL: empHrs=8; System.out.println("----------");System.out.println("Employee is Present Full time");break;
 				case IS_PRESENT_HALF:empHrs =4;System.out.println("----------");System.out.println("Employee is Present Part Time");break;
 				default:System.out.println("Employee is Absent");
 			}
-			Daily_Wage = Wage_Per_Hour * empHrs;
-			Monthly_Wage+= Daily_Wage;
-			System.out.println("Daily Wage of Employee is " + Daily_Wage);
+			totalempHrs +=empHrs;
+			System.out.println(" Day " + workingDays + ": hours " +empHrs );
 		}
+		Monthly_Wage= totalempHrs * Wage_Per_Hour;
 		System.out.println("Monthly Wage of Employee is " + Monthly_Wage);
 	}
 }
