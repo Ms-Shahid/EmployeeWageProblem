ba0011cb83dc5a75a5500b4fec95802c761a9cb6
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-20 23:46:37 +0530

    daily wage based on full & part time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,19 +4,27 @@ public class EmployeeWage {
 
 	static int IS_FULL_TIME =1;
 	static int Wage_Per_Hour = 20;
+	static int IS_PRESENT_FULL =1;
+	static int IS_PRESENT_HALF = 2;
 	public static void main(String[] args) {
 		
-		int Total_Hour = 8;
+		int Full_Time_Work = 8;
 		int Daily_Wage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		int Part_Time_Work = 4;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
-		if (empCheck == IS_FULL_TIME) {
-			Daily_Wage = Wage_Per_Hour * Total_Hour;
-			System.out.println("Employee is Present");
-			System.out.println("Daily Wage of Employee is " + Daily_Wage);
-		}else {
-			System.out.println("Employee is Absent");
-			System.out.println("Daily Wage of Employee is " + Daily_Wage);
+		if (empCheck == IS_PRESENT_FULL) {
+			Daily_Wage = Wage_Per_Hour * Full_Time_Work;
+			
+			System.out.println("Employee is Present for full-time");	
 		}
+		else if (empCheck == IS_PRESENT_HALF ) {
+			Daily_Wage = Wage_Per_Hour * Part_Time_Work;
+			System.out.println("Employee is Present for part-time");	
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+		System.out.println("Daily Wage of Employee is " + Daily_Wage);
 	}
 }
